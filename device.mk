95e3254cc46c4f8579882e8881ba651ed34e9d8a
eifal <syahrifal7@gmail.com>
2024-07-24 10:39:03 +0700

    "Update miuicamera.mk to config.mk in device.mk"

--- device.mk
+++ device.mk
@@ -30,7 +30,7 @@ PRODUCT_PACKAGES += \
     libpiex_shim
 
 # Miui Camera
-$(call inherit-product-if-exists, vendor/xiaomi/camera/miuicamera.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
